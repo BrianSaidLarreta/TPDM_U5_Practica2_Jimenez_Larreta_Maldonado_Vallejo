69d5141ff48129ee9780482ca2e0c38520cd3f15
Brian <brsalarretaor@ittepic.edu.mx>
2019-05-24 15:20:25 -0600

    Piedra Papel Tijera

---
+++ app/src/main/java/com/example/tpdm_u5_practica2_larreta_maldonado_jimenez_vallejo/Juego.java
@@ -0,0 +1,13 @@
+package com.example.tpdm_u5_practica2_larreta_maldonado_jimenez_vallejo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Juego extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juego);
+    }
+}
